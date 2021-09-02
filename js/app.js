a66a841a09c0882859d3eb2e43cd27328ad8a640
Mehedihasan023 <mh20973@gmail.com>
2021-09-03 01:07:08 +0600

    display poperty edited

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 const API_KEY = `bd18ec3299834970d0c62b79d7180060`;
 
+document.getElementById('city').style.display='none';
+document.getElementById('weather-icon').style.display='none';
+document.getElementById('condition').style.display='none';
+document.getElementById('temperature').style.display='none';
+document.getElementById('celsius').style.display='none';
 const searchTemperature =()=>{
     const city = document.getElementById('city-name').value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
@@ -16,10 +21,17 @@ const displayTemperature = temperature =>{
     setInnerText('city',temperature.name);
     setInnerText('temperature',temperature.main.temp);
     setInnerText('condition',temperature.weather[0].main);
-    console.log(temperature);
     
     // set weather icon 
     const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`
     const imgIcon =document.getElementById('weather-icon');
     imgIcon.setAttribute('src',url);
+
+
+    
+document.getElementById('city').style.display='block';
+document.getElementById('weather-icon').style.display='inline';
+document.getElementById('condition').style.display='block';
+document.getElementById('temperature').style.display='inline';
+document.getElementById('celsius').style.display='inline';
 }
